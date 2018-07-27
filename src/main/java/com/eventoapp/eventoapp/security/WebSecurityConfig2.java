ea00629f61be47713b30be2021cfad858c5cf21c
Rafael Sanzio <rafael.sanzio18@gmail.com>
2018-07-27 17:35:36 -0400

    Nova Parte

--- src/main/java/com/eventoapp/eventoapp/security/WebSecurityConfig2.java
+++ src/main/java/com/eventoapp/eventoapp/security/WebSecurityConfig2.java
@@ -1,6 +1,5 @@
 package com.eventoapp.eventoapp.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
@@ -9,34 +8,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Profile("prod")
+@Profile("dev")
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private ImplementsUserDetailsService userDetailsService;
+public class WebSecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().permitAll()
-                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
+        auth.inMemoryAuthentication().withUser("michelli")
+                .password("123").roles("ADMIN");
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/materialize/**", "/style/**");
+        web.ignoring().antMatchers("/materialize", "/style/**");
     }
 }
